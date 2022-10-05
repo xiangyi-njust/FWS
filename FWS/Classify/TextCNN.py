24e020898bd53dd5790d5dbd0931495007d0ebbf
熠 向 <2949410098@qq.com>
2022-10-05 21:29:22 +0800

    本地代码提交的注释说明

---
+++ FWS/Classify/TextCNN.py
@@ -0,0 +1,87 @@
+#from msilib.schema import Class
+from tensorflow.keras.layers import Input,Embedding,Conv1D,MaxPool1D,concatenate,Flatten,Dropout
+from tensorflow.keras.layers import Dense,MaxPooling1D,Layer
+from tensorflow.keras.models import Model
+from tensorflow.keras.preprocessing.text import Tokenizer
+import pandas as pd
+import tensorflow as tf
+import math
+from tensorflow import keras
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+import numpy as np
+import sklearn.metrics as sm
+from sklearn.model_selection import train_test_split
+
+class TextCNN():
+    def __init__(self,shell_args):
+        self.model = None
+        self.tokenizer = None
+        self.x = None
+        self.y = None 
+        
+        self.path = shell_args['path']
+        self.batch_size = shell_args['batch_size']
+        self.epochs = shell_args['epochs']
+        self.isTrained = shell_args['isTrained']
+        self.model_save_path = shell_args['model_save_path']
+
+    def net(self):
+        main_input = Input(shape=(40,))
+        # word embedding（使用预训练的词向量）
+        embed = Embedding(len(self.tokenizer.word_index) + 1, 300, input_length=40)(main_input)
+        # 词窗大小分别为3,4,5
+        cnn1 = Conv1D(200, 3, padding='same', strides=1, activation='relu')(embed)
+        cnn1 = MaxPooling1D(pool_size=38)(cnn1)
+        cnn2 = Conv1D(200, 4, padding='same', strides=1, activation='relu')(embed)
+        cnn2 = MaxPooling1D(pool_size=37)(cnn2)
+        cnn3 = Conv1D(200, 5, padding='same', strides=1, activation='relu')(embed)
+        cnn3 = MaxPooling1D(pool_size=36)(cnn3)
+        # 合并三个模型的输出向量
+        cnn = concatenate([cnn1, cnn2, cnn3], axis=-1)
+        flat = Flatten()(cnn)
+        drop = Dropout(0.2)(flat)
+        main_output = Dense(6, activation='softmax')(drop)
+        
+        self.model = Model(inputs=main_input, outputs=main_output)
+    
+    def readData(self):
+        data = pd.read_excel(self.path)
+        self.tokenizer = Tokenizer()
+        self.tokenizer.fit_on_texts(data.text.values.tolist())
+
+        y_labels = [str(item) for item in data.CAT.value_counts().index.tolist()]
+        y_labels_to_id = dict([(item, i)for i,item in enumerate(y_labels)]) 
+
+        self.x = pad_sequences(self.tokenizer.texts_to_sequences(data.text.values.tolist()),maxlen=40,padding='post')
+        #将一个label数组转化成one-hot数组。
+        self.y = np.eye(len(y_labels_to_id))[[y_labels_to_id[str(item)] for item in data.CAT.values.tolist()]]
+
+    def train(self):
+        if self.isTrained == False:
+            # data
+            train_x,test_x,train_y,test_y = train_test_split(self.x,self.y,test_size=0.1,random_state=11)
+
+            # model setting
+            self.net()
+            self.model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+            early_stop = tf.keras.callbacks.EarlyStopping(monitor='val_accuracy', patience=3)
+            
+            # train 
+            print()
+            print("**********START TRAIN*******************")
+            print()
+            self.model.fit(train_x, train_y, batch_size=self.batch_size, epochs=self.epochs,
+            validation_data=(test_x,test_y),callbacks=[early_stop])
+            self.model.save(self.model_save_path)
+            print()
+            print("**********END TRAIN*******************")
+        else:
+            self.model = keras.models.load_model(self.model_save_path)
+        
+    def test(self):
+        print()
+        print("predict result:")
+        _,test_x,_,test_y = train_test_split(self.x,self.y,test_size=0.1,random_state=11)
+
+        preds = self.model.predict(test_x)
+        print(sm.classification_report([np.argmax(item) for item in test_y], [np.argmax(item) for item in preds],digits=4))
---
+++ FWS/Extract/main.py
@@ -0,0 +1,195 @@
+from xmlrpc.client import Boolean
+import sklearn.metrics as sm
+import pickle
+import pandas as pd
+import argparse
+import inspect
+from ast import Interactive
+from sklearn.svm import LinearSVC
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegression as LR
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB, ComplementNB, BernoulliNB
+from sklearn.feature_selection import VarianceThreshold,SelectKBest,RFE,SelectFromModel,chi2
+from sklearn.model_selection import train_test_split,cross_val_score,cross_validate
+import re
+from nltk.corpus import stopwords
+from nltk import word_tokenize, pos_tag
+from nltk.corpus import wordnet
+from nltk.stem import WordNetLemmatizer
+
+# user's intention can divide two types:
+# 1. use model to extract fws  -- my target is not package a tool for others to use
+# 2. review our experiment   -- my first intention
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--flag',type=Boolean,default=False)
+parser.add_argument('--ori_path',type=str)
+parser.add_argument('--tar_path',type=str)
+parser.add_argument('--model',type=str)
+parser.add_argument('--cv',type=int)
+
+def remove_punctuation(line):
+    line = str(line)
+    line = line.strip()
+    line = line.replace('-', ' ')
+    line = line.replace('\'s', '')
+    rule2 = re.compile(u"\\(.*?\\)|{.*?}|\\[.*?]")
+    line = rule2.sub('', line)
+    rule1 = re.compile(u"[^a-zA-Z\\s+]")
+    line = rule1.sub('', line).lower()
+    return line
+
+def get_wordnet_pos(tag):
+    if tag.startswith('J'):
+        return wordnet.ADJ
+    elif tag.startswith('V'):
+        return wordnet.VERB
+    elif tag.startswith('N'):
+        return wordnet.NOUN
+    elif tag.startswith('R'):
+        return wordnet.ADV
+    else:
+        return None
+
+def lemm(sentence):
+    tokens = word_tokenize(sentence)  # 分词
+    tagged_sent = pos_tag(tokens)     # 获取单词词性
+    wnl = WordNetLemmatizer()
+    lemmas_sent = []
+    for tag in tagged_sent:
+        wordnet_pos = get_wordnet_pos(tag[1]) or wordnet.NOUN
+        lemmas_sent.append(wnl.lemmatize(tag[0], pos=wordnet_pos)) # 词形还原
+    return ' '.join(lemmas_sent)
+
+def preprocess(texts):
+    re_texts = [remove_punctuation(text) for text in texts]
+    le_texts = [lemm(text) for text in re_texts]
+    return texts
+
+def getTfidf(ori_path,tar_path):
+    # read data
+    df = pd.read_excel(ori_path)
+    texts = df['text'].tolist()
+    labels = df['label'].tolist()
+    
+    texts = preprocess(texts)
+    # extract the tfidf feature
+    vectorizer = TfidfVectorizer(ngram_range=(1, 3), min_df=3, max_df=1.0, sublinear_tf=True, analyzer='word',)
+    vectorizer.fit(texts)
+    texts = vectorizer.transform(texts)
+
+    # dump
+    data = (texts, labels)
+    fp = open(tar_path, 'wb')
+    pickle.dump(data, fp)
+    fp.close()
+
+def evaluate(clf,texts,labels,cv):
+    scores = cross_validate(clf,texts,labels,cv=cv,n_jobs=-1, \
+            scoring=['precision_macro','recall_macro','f1_macro'])
+
+    print("macro result: ")
+    print(" precision scores:")
+    print(scores['test_precision_macro'])
+    print(" mean scores : ",scores['test_precision_macro'].mean())
+    print()
+    print(" recall_macro scores:")
+    print(scores['test_recall_macro'])
+    print(" mean scores : ",scores['test_recall_macro'].mean())
+    print()
+    print(" f1_macro scores:")
+    print(scores['test_f1_macro'])
+    print(" mean scores : ",scores['test_f1_macro'].mean())
+    print()
+
+def svm(texts,labels,cv=10):
+    clf = LinearSVC(max_iter=5000, C=15,)
+    selector = RFE(clf, n_features_to_select=5200, step=5000).fit(texts, labels)
+    texts = selector.transform(texts)
+    evaluate(clf,texts,labels,cv)
+
+def nb(texts,labels,cv=10):
+    clf = BernoulliNB(alpha=0.0001,)
+    texts = SelectFromModel(clf, norm_order=1).fit_transform(texts, labels)
+    evaluate(clf,texts,labels,cv)
+
+def rf(texts,labels,cv=10):
+    clf = RandomForestClassifier(n_estimators=200)
+    selector = RFE(clf, n_features_to_select=5200, step=5000).fit(texts, labels)
+    texts = selector.transform(texts)
+    evaluate(clf,texts,labels,cv)
+
+def knn(texts,labels,cv=10):
+    clf = KNeighborsClassifier(n_neighbors=12)
+    selector = VarianceThreshold(0.00001)
+    texts = selector.fit_transform(texts, labels)
+    texts = SelectKBest(chi2, k=90).fit_transform(texts, labels)
+    evaluate(clf,texts,labels,cv)
+
+def lr(texts,labels,cv=10):
+    clf = LR(max_iter=1000)
+    selector = RFE(clf, n_features_to_select=5200, step=5000).fit(texts, labels)
+    texts = selector.transform(texts)
+    evaluate(clf,texts,labels,cv)
+
+def help(): 
+    print()
+    print("*****************************************************************")
+    print("*  This python script will help you to understand our experiment")
+    print("*  we use five machine learning models to extract fws from paper")
+    print("*  follow below's commnd,you can see the model's performance in our dataset")
+    print("*  Notice!: when you read the source code you will find that some parameters \
+about the classifier has configure,these parameters are getted by our multiple experiments")
+    print()
+    print("*  command example:")
+    print("python extract.py --model svm --cv 10 ")
+    print()
+    print("*  parameters meaning:")
+    print("--model: configure the classifier you want to use")
+    print("--cv: configure the n-fold cross-val")
+    print()
+    print("for any problem,you can contact us with this email:xiangyi@njust.edu.cn")
+    print("*****************************************************************")
+
+def main():
+    # shell args get 
+    args = parser.parse_args()
+    shell_args = args._get_kwargs()
+    arg_val = {}
+    for arg,val in shell_args:
+        arg_val[arg] = val
+    
+    cv = arg_val['cv']
+    flag = arg_val['flag']
+
+    # get tfidf
+    if arg_val['model'] == None:
+        help()
+    else:
+        if flag == False:
+            getTfidf(arg_val['ori_path'],arg_val['tar_path'])
+        data_fp = open(arg_val['tar_path'], 'rb')
+        texts, labels = pickle.load(data_fp)
+        data_fp.close()
+    
+    # chooose model
+    if arg_val['model'] == 'svm':
+        svm(texts,labels,cv)
+    elif arg_val['model'] == 'random-forest':
+        rf(texts,labels,cv)
+    elif arg_val['model'] == 'knn':
+        knn(texts,labels,cv)
+    elif arg_val['model'] == 'logistic-regression':
+        lr(texts,labels,cv)
+    elif arg_val['model'] == 'naive-bayes':
+        nb(texts,labels,cv)
+    else:
+        exit()
+
+if __name__ == '__main__':
+    main()
+
+
+
